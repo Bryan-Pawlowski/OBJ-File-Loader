3d6905d8ca15eb89c25f3b3973004f0ffbb1ddbe
Bryanayrb <Bryan.s.pawlowski@gmail.com>
2014-10-31 12:22:39 -0700

    Shader loader almost finished added shaders

--- OBJ-File-Loader/OBJ-File-Loader/testApp.cpp
+++ OBJ-File-Loader/OBJ-File-Loader/testApp.cpp
@@ -12,17 +12,33 @@
 #include <windowsx.h>
 #include <sstream>
 #include <string>
+#include <D3D11.h>
+#include <D3DX11.h>
+#include <D3DX10.h>
 #include "OBJ-loader.h"
 
 using namespace std;
 
+//globals
+IDXGISwapChain *swapchain;
+ID3D11Device *dev;
+ID3D11DeviceContext *devcon;
+ID3D11RenderTargetView *backbuffer;
+ID3D11VertexShader * pVS;
+ID3D11PixelShader * pPS;
+
+//prototypes
 void createModelTests(void);
 int testDX( HINSTANCE hInstance, int nCmdShow );
+void InitD3D(HWND hWnd);
+void CleanD3D(void);
+void RenderFrame(void);
+void InitPipeline( void );
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 
-#define SCREEN_WIDTH	500
-#define SCREEN_HEIGHT	400
+#define SCREEN_WIDTH	800
+#define SCREEN_HEIGHT	600
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
@@ -78,17 +94,28 @@ int testDX(HINSTANCE hInstance, int nCmdShow)
 							hInstance,  //handle for our app
 							NULL);
 
+	InitD3D( hWnd );
+
+
 	ShowWindow(hWnd, nCmdShow);
 
-	MSG msg;
+	MSG msg = { 0 };
 
-	while(GetMessage(&msg, NULL, 0, 0))
-	{
+	while(TRUE)
+	{	
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
 			TranslateMessage(&msg);
 
 			DispatchMessage(&msg);
 
+			if (msg.message == WM_QUIT) break;
+		}
+		else RenderFrame();
 	}
+
+	CleanD3D();
 
 	return msg.wParam;
 }
@@ -113,13 +140,100 @@ void createModelTests(void)
 	//test for correct failure
 	Model *testModel = new Model;
 	int fail = testModel->modelInit("null.obj");
-	if (!fail) MessageBox(HWND_DESKTOP, L"Self-deletion on fail test failed!", L"Test1", MB_OK);
+	if (!fail){
+		MessageBox(HWND_DESKTOP, L"Self-deletion on fail test failed!", L"Test1", MB_OK);
+		delete testModel;
+		exit(EXIT_FAILURE);
+	}
 	delete testModel;
-
+	//ZeroMemory(testModel, sizeof(Model));
 	//test for open success
 	testModel = new Model;
 	int res = testModel->modelInit("cube.obj");
-	if (res == 1) MessageBox(HWND_DESKTOP, L"Could not find or open the cube.obj test file.", L"Test2", MB_OK);	
-
+	if (res == 1){
+		MessageBox(HWND_DESKTOP, L"Could not find or open the cube.obj test file.", L"Test2", MB_OK);
+		delete testModel;
+		exit(EXIT_FAILURE);
+	}
 	delete testModel;
+}
+
+void InitD3D(HWND hWnd)
+{
+	DXGI_SWAP_CHAIN_DESC scd;
+
+	ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+	scd.BufferCount = 1;
+	scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	scd.OutputWindow = hWnd;
+	scd.SampleDesc.Count = 4;
+	scd.SampleDesc.Quality = 0;
+	scd.Windowed = TRUE;
+	scd.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+
+	D3D11CreateDeviceAndSwapChain(NULL,
+		D3D_DRIVER_TYPE_HARDWARE,
+		NULL,
+		NULL,
+		NULL,
+		NULL,
+		D3D11_SDK_VERSION,
+		&scd,
+		&swapchain,
+		&dev,
+		NULL,
+		&devcon);
+
+	ID3D11Texture2D *pBackBuffer;
+	swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+	dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+	pBackBuffer->Release();
+
+	devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+	D3D11_VIEWPORT viewport;
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = SCREEN_WIDTH;
+	viewport.Height = SCREEN_HEIGHT;
+
+	devcon->RSSetViewports(1, &viewport);
+}
+
+void CleanD3D() //Release all of our COM objects.
+{
+	swapchain->SetFullscreenState(FALSE, NULL);
+
+	swapchain->Release();
+	dev->Release();
+	devcon->Release();
+	backbuffer->Release();
+}
+
+void RenderFrame(void)
+{
+
+	devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR(0.0f, 0.2f, 0.4f, 1.0f));
+
+	swapchain->Present(0, 0);
+}
+
+void InitPipeline(void)
+{
+	ID3D10Blob *VS, *PS;
+	D3DX11CompileFromFile(L"VShader.hlsl", 0, 0, "VShader", "vs_5_0", 0, 0, 0, &VS, 0, 0);
+	D3DX11CompileFromFile(L"PShader.hlsl", 0, 0, "PShader", "ps_5_0", 0, 0, 0, &PS, 0, 0);
+
+	dev->CreateVertexShader(VS->GetBufferPointer(), VS->GetBufferSize(), NULL, &pVS);
+	dev->CreatePixelShader(PS->GetBufferPointer(), PS->GetBufferSize(), NULL, &pPS);
+
+
+	devcon->VSSetShader(pVS, 0, 0);
+	devcon->PSSetShader(pPS, 0, 0);
 }
